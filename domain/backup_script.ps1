5f8cdddb8ee9e1bafb5089765c840b2a0b7f08db
meir franco <meir.franco@mail.huji.ac.il>
2017-04-25 13:00:53 +0000

    Added: a creation of a log file to document the backup process...

--- domain/backup_script.ps1
+++ domain/backup_script.ps1
@@ -1,13 +1,26 @@
 $backupFolder = "U:\Backup\" 
 $suffix_to_backup=".ssh","Sources",".Py*"
-#$backuped_dirs = New-Object System.Collections.ArrayList 
+#$backuped_dirs = New-Object System.Collections.ArrayList
+
+$user_name = Get-ChildItem Env:USERNAME
+$user_name = $user_name.value
+$logFile = "C:\Users\$user_name\backup_log.txt"
+$date = Get-Date
 
 if(!(Test-Path -Path $backupFolder ))
 {
     New-Item -ItemType directory -Path $backupFolder
 }
-
+if(!(Test-path -Path $logFile))
+{
+	New-Item -Path "C:\Users\$user_name" -ItemType "file" -Name "backup_log.txt"
+}
+"################################################"|Add-Content $logFile
+$date|Add-Content $logFile
+"Started backup process..."| Add-Content $logFile
 foreach ($suffix in $suffix_to_backup){
+	
+	"Copying " + $suffix + " files..."|Add-Content $logFile
 	if ($suffix.EndsWith("*"))
 	{
 		$dirs = Get-ChildItem "$ENV:USERPROFILE\$suffix*"
@@ -24,10 +37,12 @@ foreach ($suffix in $suffix_to_backup){
 		{
 			$base_name=$suffix
 		}
+		"Copied " + $base_name|Add-Content $logFile
 		#echo "$dir ,  $base_name"
 		Robocopy $dir  $backupFolder\$base_name /MIR /xo /fft  /mt:7 /w:3 /r:3 /nfl /ndl /nc /ns 
 		#[void] $backuped_dirs.Add($base_name)
-	} 	
+	}
+	"Finished copying " + $suffix + " files..."|Add-Content $logFile
 }
 
 copy $profile $backupFolder\profile.ps1
