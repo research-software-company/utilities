0d1b4bedc878b72ee055dbf3d06e857c82f12729
Meir Franco <meir.franco@mail.huji.ac.il>
2017-08-07 15:02:17 +0300

    Update - copies ALL the files found in ~ directory into the backup directory

--- domain/restore_script.ps1
+++ domain/restore_script.ps1
@@ -1,35 +1,22 @@
 $backupFolder = "U:\Backup\" 
-$dirs = Get-ChildItem $backupFolder |% {$_.BaseName}
-$source_dir = "$ENV:USERPROFILE\Sources"
-#find data dir
-#$data_drive_name = GET-WMIOBJECT -query 'SELECT * from win32_logicaldisk where DriveType = "3" AND VolumeName = "Data"' | Select-Object "DeviceID"
-if(!(Test-Path -Path $source_dir ))
-{
-	throw "$source_dir does not exist"
-}
-$target = Get-Item $source_dir | Select-Object -ExpandProperty Target
-if (! $target)
-{
-	throw "$source_dir is not a link"
-}
-	
-foreach ($dir in $dirs)
-{
-	#copy to target
-	$is_sources=$dir.CompareTo("Sources")
-	if ($is_sources -eq 0)
-	{
-		Robocopy $backupFolder\$dir $target /MIR /xo /fft  /mt:7 /w:3 /r:3 /nfl /ndl /nc /ns 
-	} 
-	Else
-	{	
-		Robocopy $backupFolder\$dir $ENV:USERPROFILE\$dir /MIR /xo /fft  /mt:7 /w:3 /r:3 /nfl /ndl /nc /ns 
-	}
-} 	
 
-$profile_dir = split-path $profile
-if (!(Test-Path -Path $profile_dir))
+$logFile = "~\restore_log.txt"
+
+if(!(Test-Path -Path $backupFolder ))
 {
-	New-Item -ItemType directory -Path $profile_dir
+    New-Item -ItemType directory -Path $backupFolder
 }
-copy $backupFolder\profile.ps1 $profile
+if(!(Test-path -Path $logFile))
+{
+	New-Item -Path "~" -ItemType "file" -Name "restore_log.txt"
+}
+"################################################"|Add-Content $logFile
+Get-Date | Add-Content $logFile
+"Started restore process..."| Add-Content $logFile
+
+Robocopy $backupFolder $ENV:USERPROFILE /MIR /xo /fft  /mt:7 /w:3 /r:3 /nfl /ndl /nc /ns 
+"Finished copying files..."|Add-Content $logFile
+
+copy $backupFolder $profile \profile.ps1
+
+"Restore process finished" | Add-Content $logFile
